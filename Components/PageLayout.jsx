bab7e07bbf6a44954bc72cc72cd62cf1059fc7f4
Alan Jair <alanuadalupe1665@gmail.com>
2022-08-21 13:18:41 -0500

    Vamos a subir el proyecto al repositorio por que instalaremos linux

--- Components/PageLayout.jsx
+++ Components/PageLayout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function PageLayout({ children , title = 'NewsApp' }){
     return(
@@ -8,8 +9,13 @@ export default function PageLayout({ children , title = 'NewsApp' }){
                 <meta name="description" content="newsapp - the best app to read news" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header>
-                newsapi
+            <header >
+                <div>
+                    <Link href='/'>newsapi</Link>
+                </div>
+                <div>
+                    <Link href='/about'>About</Link>
+                </div>
             </header>
             <main>
             { children }
@@ -17,6 +23,8 @@ export default function PageLayout({ children , title = 'NewsApp' }){
             <style jsx>{
             `
             header{
+            display: flex;
+            justify-content: space-between;
             padding: 20px;
             }
             `
--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,7 @@ import PageLayout from '../Components/PageLayout'
 import { useState, useEffect } from 'react'
 
 
-export default function Home() {
-
-  const [articles, setArticles] = useState([])
-
-  useEffect(() => {
-    fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-07-20&sortBy=publishedAt&apiKey=400562fcc76d4ad6944fa59a6de2542d`)
-    .then( res => res.json())
-    .then(response => {
-      const { articles } = response
-      setArticles(articles)
-    })
-  }, [])
+export default function Home({ articles }) {
 
   return (
     <PageLayout title='NewsApp - Home'>
@@ -25,12 +14,15 @@ export default function Home() {
     <Head>
       <title> </title>
     </Head>
-      { articles.length === 0 && <p>Loading..</p>}
+      { articles.length === 0 && <p>No tenemos articulos</p>}
       { articles.length > 0 && articles.map((article, index) => (
         <div key={index}>
-          <img 
-          src={article.urlToImage} 
-          alt={`Image for the article ${article.title}`} />
+          <Image
+          src={article.urlToImage}
+          alt={`Image for the article ${article.title}`}
+          width={450}
+          height={300}
+          />
           <h2>{article.title}</h2>
           <p>{ article.description}</p>
         </div>
@@ -38,4 +30,26 @@ export default function Home() {
     </div>
     </PageLayout>
   )
+}
+
+// export async function getStatic(){
+//   const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-07-20&sortBy=publishedAt&apiKey=400562fcc76d4ad6944fa59a6de2542d`)
+//   const { articles } = await response.json()
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
+
+
+// ESTE METODO SE EJECUTA EN EL SERVIDOR
+export async function getServerSideProps(context){
+  const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-07-20&sortBy=publishedAt&apiKey=400562fcc76d4ad6944fa59a6de2542d`)
+  const { articles } = await response.json()
+  return {
+    props: {
+      articles
+    }
+  }
 }
